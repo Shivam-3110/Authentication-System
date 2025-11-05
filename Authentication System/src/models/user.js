3ef6c2de48ebd605505d7575202eabb0cff76c80
Shivam-3110 <mrshivam00022@gmail.com>
2025-11-05 07:40:21 +0530

    user Model created

---
+++ Authentication System/src/models/user.js
@@ -0,0 +1,25 @@
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email :{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    role : {
+        type:String,
+        default:"user",
+    },
+},
+    {timestamps:true},
+);
+
+export default mongoose.model("User",userSchema)
