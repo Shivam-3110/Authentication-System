df7b7a5397559f6021d7b12538330f10f647738e
Shivam-3110 <mrshivam00022@gmail.com>
2025-11-08 07:49:48 +0530

    register and login api fixed

--- Authentication System/src/app.js
+++ Authentication System/src/app.js
@@ -2,7 +2,10 @@
 import express from "express";
 import AuthRoutes from "./routes/auth.routes.js";
 const app = express();
-app.use(express.json);
-app.use('/')
+app.use(express.json());
+app.get('/',(req , res)=>{
+    res.send({message:"hello world"});
+});
+app.use('/auth',AuthRoutes);
 
 export default app;
--- Authentication System/src/routes/auth.routes.js
+++ Authentication System/src/routes/auth.routes.js
@@ -1,13 +1,41 @@
 import express from "express";
-import userModel from "../models/user";
+import userModel from "../models/user.js";
 const router = express.Router()
 router.post('/register',async(req,res)=>{
     const{username,email,password,role} = req.body;
 
     const user = await userModel.create({
-        
+        username,email,password,role
+    })
+    res.status(201).json({
+        message:"user registered successfully",
+        user
     })
 })
+
+router.post('/login',async(req,res)=>{
+    const {username,password} = req.body
+    const isUserExists = await userModel.findOne({
+        username:username
+    })
+    if(!isUserExists){
+        return res.status(401).json({
+            message:"user account not found"
+        })
+    }
+    const isPasswordValid = password ==isUserExists.password
+
+    if(!isPasswordValid){
+        return res.status(401).json({
+            message:"Invalid password"
+        })
+    }
+
+    res.status(200).json({
+        message:"user loggedIn Successfully"
+    });
+})
+
 
 
 
