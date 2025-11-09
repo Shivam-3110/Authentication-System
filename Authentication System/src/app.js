29fca6c0fddc682bae47abf7a07f6dc9c893756c
Shivam-3110 <mrshivam00022@gmail.com>
2025-11-09 18:48:05 +0530

    fixed jwt and user api

--- Authentication System/src/app.js
+++ Authentication System/src/app.js
@@ -1,8 +1,10 @@
 
 import express from "express";
 import AuthRoutes from "./routes/auth.routes.js";
+import cookieparser from "cookie-parser";
 const app = express();
 app.use(express.json());
+app.use(cookieparser)
 app.get('/',(req , res)=>{
     res.send({message:"hello world"});
 });
--- Authentication System/src/routes/auth.routes.js
+++ Authentication System/src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import userModel from "../models/user.js";
+import jwt from "jsonwebtoken";
 const router = express.Router()
 router.post('/register',async(req,res)=>{
     const{username,email,password,role} = req.body;
@@ -7,9 +8,14 @@ router.post('/register',async(req,res)=>{
     const user = await userModel.create({
         username,email,password,role
     })
+    const token = jwt.sign({
+        id:user._id,
+    },process.env.JWT_SECRET)
+    res.cookie("token",token)
     res.status(201).json({
         message:"user registered successfully",
         user
+        
     })
 })
 
@@ -34,6 +40,32 @@ router.post('/login',async(req,res)=>{
     res.status(200).json({
         message:"user loggedIn Successfully"
     });
+})
+
+router.get('/users',async(req,res)=>{
+    const {token} = req.cookies;
+    if(!token){
+        return res.status(401).json({
+            message:"Unauthorized access"
+        })
+    }
+    jwt.verify(token , process.env.JWT_SECRET)
+
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        const user = await userModel.findOne({
+            _id:decoded.id 
+        }).select("-password -v").lean()
+        res.status(200).json({
+            message:"user data fetched Successfully",
+            user
+        })
+
+    } catch (error) {
+        return res.status(401).json({
+            message:"Unauthorized- Invalid Token"
+        })
+    }
 })
 
 
